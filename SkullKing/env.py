1e4f52983b5a1aab810115b9754a73be072c60b0
Michael Blum <mab8192@gmail.com>
2023-02-25 22:05:56 -0700

    started setting up env

---
+++ SkullKing/env.py
@@ -0,0 +1,28 @@
+import gym
+import numpy as np
+
+
+class Card:
+    def __init__(self):
+        pass
+
+class Hand:
+    def __init__(self):
+        pass
+
+class Deck:
+    def __init__(self):
+        pass
+
+class SkullKingEnv(gym.Env):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def reset(self) -> None:
+        pass
+
+    def _get_obs(self):
+        pass
+
+    def step(self, action):
+        pass
