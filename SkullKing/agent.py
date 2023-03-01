209e468f35e4d5c140e2e38751cc99bb1e6752a1
Michael Blum <49876653+mab8192@users.noreply.github.com>
2023-03-01 13:10:08 -0700

    separated out base agent properties to a base class

--- SkullKing/agent.py
+++ SkullKing/agent.py
@@ -2,6 +2,7 @@ import random
 from collections import deque
 from typing import List
 from copy import deepcopy
+import numpy as np
 
 from game import Hand, Trick
 
@@ -20,9 +21,9 @@ class MemoryReplay:
         return len(self.memory)
 
 
-class Agent:
+class BaseAgent:
     """An agent that learns with delayed rewards."""
-    def __init__(self, id: int, memory_capacity: int = 10_000) -> None:
+    def __init__(self, id: int) -> None:
         # Per game properties
         self.id = id
         self.score = 0
@@ -32,21 +33,14 @@ class Agent:
         self.starting_hand = Hand()
         self.hand = Hand()  # Current hand of cards
         self.tricks: List[Trick] = []  # List of collected tricks so far
-        self.last_action = -1
-        self.last_state = None
 
-        # DQNs
-        self.bid_network = None  # Network that bids on the current round
-        self.play_network = None  # Network that decides which card to play
-
-        self.bid_memory = MemoryReplay(memory_capacity)
-        self.play_memory = MemoryReplay(memory_capacity)
+        self.current_position = 0  # Position in play
 
         # TODO: future properties
         self.loot_with = -1  # Flag to indicate if a player has loot with anyone
         self.wager = 0  # Ability to wager with Rascal of Roatan
 
-    def compute_score_for_round(self, round_number: int):
+    def compute_score(self, round_number: int):
         # Special case: bet 0
         if self.bet == 0:
             if len(self.tricks) == 0:
@@ -73,22 +67,70 @@ class Agent:
         self.tricks = []
         self.bet = -1
 
-    def bid(self):
-        """Make a bid prediction based on the current player's hand."""
-        self.bet = 0
-
-    def _get_legal_actions(self, state):
+    def _get_legal_actions(self, game_state):
         """Of the cards in this agent's hand, return only those which are legal to play."""
         pass
 
+    def bid(self):
+        """Make a bid prediction based on the current player's hand."""
+        raise NotImplementedError
+
+    def play(self, game_state):
+        """Play a card from the agent's hand, given the current global state and the agent's internal state."""
+        raise NotImplementedError
+
+
+class RLAgent(BaseAgent):
+    """An agent that learns with delayed rewards."""
+    def __init__(self, id: int, memory_capacity: int = 10_000) -> None:
+        super().__init__(id)
+
+        # Extra properties for RL
+        self.last_action = -1
+        self.last_state = None
+
+        # Neural Networks
+        self.bid_network = None  # Network that bids on the current round, trained with supervised learning
+
+        self.play_network = None  # Network that decides which card to play, trained with Deep Q-Learning
+        self.target_network = None
+
+        self.bid_memory = MemoryReplay(memory_capacity)
+        self.play_memory = MemoryReplay(memory_capacity)
+
+    def cleanup(self):
+        super().cleanup()
+        self.last_action = -1
+        self.last_state = None
+
+    def bid(self):
+        """Make a bid prediction based on the current player's hand."""
+        raise NotImplementedError
+
     def play(self, state):
         """Play a card from the agent's hand, given the current global state and the agent's internal state."""
-        self.last_state = state
+        raise NotImplementedError
 
     def reward(self, reward):
         """Give the agent a reward for the actions played during the current round. Add experiences to memory"""
-        self.bid_memory.push((self.starting_hand, self.bet, len(self.tricks)))
+        self.bid_memory.push((self.starting_hand, len(self.tricks)))
         self.play_memory.push((self.last_state, self.last_action, reward))
 
     def optimize(self):
-        pass
+        raise NotImplementedError
+
+
+class RandomAgent(BaseAgent):
+    """An agent that learns with delayed rewards."""
+    def __init__(self, id: int) -> None:
+        super().__init__(id)
+
+    def bid(self):
+        """Make a bid prediction based on the current player's hand."""
+        self.bet = random.randint(0, len(self.hand))
+
+    def play(self, game_state):
+        """Play a card from the agent's hand, given the current global state and the agent's internal state."""
+        legal_actions = self._get_legal_actions(game_state)
+        action = np.random.choice(legal_actions)
+        return action
